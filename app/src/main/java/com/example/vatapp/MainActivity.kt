c030c440b2c18d4d598440736cbe1e62ad7c06b0
Shaja3 <defrent1999@gmail.com>
2022-08-08 13:03:35 +0300

    Exercise 2 - WEEK 2

--- app/src/main/java/com/example/vatapp/MainActivity.kt
+++ app/src/main/java/com/example/vatapp/MainActivity.kt
@@ -4,28 +4,51 @@ import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.vatapp.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
-    // binding object
     private lateinit var binding : ActivityMainBinding
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // TODO [2] Layout View Binding
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root //reference layout file
         setContentView(view) // link layout
 
+        // TODO [3] Button binding and Calculate VAT
         binding.btnCalculateVat.setOnClickListener {
             calculateVat()
         } //End setOnClickListener
+
     } // End onCreate
 
     private fun calculateVat() {
-        TODO("Not yat implemented")
+        val stringInTextField = binding.etTotalCost.text.toString()
+        val cost = stringInTextField.toDouble()
+
+        val selectedId = binding.rbVatOption.checkedRadioButtonId
+
+        val vatPercentage = when(selectedId){
+            R.id.rb_vat_10 -> 0.10
+            R.id.rb_vat_15 -> 0.15
+            else -> 0.20
+        }
+
+        val vat = vatPercentage * cost
+        var total = cost + vat
+
+        val roundVat = binding.switchRoundUp.isChecked
+
+        if (roundVat){
+            total = kotlin.math.ceil(total)
+        }
+
+        val formattedTotal = NumberFormat.getCurrencyInstance().format(total)
+        binding.txtCostTotal.text = getString(R.string.total_amount, formattedTotal)
     }
 } // End MainActivity
-
 
 
